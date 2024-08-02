44a4fb3a067ccc0569a9cd896cd4dc97602e61bb
inga-divra <ingady@gmail.com>
2024-08-02 19:59:44 +0300

    Added Modal component

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import CartContainer from "./components/CartContainer";
 import Navbar from "./components/Navbar";
 import { useEffect } from "react";
 import { calculateTotals } from "./features/cart/cartSlice";
+import Modal from "./components/Modal";
 
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
   }, [cartItems])
 
   return <main>
+    <Modal />
     <Navbar />
     <CartContainer />
   </main>
