9d37723890a3e8503f274d7cd29a4493bbf24cc4
inga-divra <ingady@gmail.com>
2024-08-02 19:12:14 +0300

    Added remove item functionality to the CartItem component

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -1,13 +1,18 @@
+import { useDispatch } from 'react-redux';
+import { removeItem } from '../features/cart/cartSlice';
 import { ChevronDown, ChevronUp } from '../icons';
 
 const CartItem = ({ id, title, price, img, amount }) => {
+    const dispatch = useDispatch()
+
     return (
         <article className='cart-item'>
             <img src={img} alt={title} />
             <div>
                 <h4>{title}</h4>
                 <h4 className='item-price'>{price} €</h4>
-                <button className='remove-btn'>
+                <button className='remove-btn'
+                    onClick={() => dispatch(removeItem(id))}>
                     remove
                 </button>
             </div>
